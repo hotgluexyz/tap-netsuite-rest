c455f8b85376fcbd4cce0fe121cde94468ea10f1
Vinicius Ghellere Sbardelotto <vinicius@hotglue.io>
2026-04-01 11:14:02 -0300

    feat(HGI-9949): add support for selected-filters.json (#74)
    * chore: use temporary HotglueSingerSDK version
    
    * feat: add support for selected filters
    
    * fix: if conditional
    
    * fix: only escape string values
    
    * fix: allow values that are not string
    
    * fix: ignore empty groups and empty IN values
    
    * chore: bump hotglue-singer-sdk

--- tap_netsuite_rest/client.py
+++ tap_netsuite_rest/client.py
@@ -573,6 +573,51 @@ class NetSuiteStream(RESTStream):
                 raise Exception(ValueError)
         return return_value
     
+    def _join_filters(self, filters):
+        return f"({' '.join(filters)})"
+
+    def _escape_quotes(self, value):
+        if isinstance(value, str):
+            escaped_value = value.replace("'", "''")
+            return f"'{escaped_value}'"
+        return value
+
+    def _parse_filters(self, filters):
+        parsed_filters = []
+        for key, value in filters.items():
+            if key.startswith("group_"):
+                group_filters = self._parse_filters(value)
+                if group_filters and len(group_filters) > 0:
+                    parsed_filters.append(self._join_filters(group_filters))
+            elif key.startswith("clause_"):
+                if value['operator'] == "EQ":
+                    parsed_filters.append(f"{value['field']} = {self._escape_quotes(value['value'])}")
+                elif value['operator'] == "IN":
+                    if isinstance(value['value'], list):
+                        if len(value['value']) == 0:
+                            continue
+                        filter_value = ", ".join(f"{self._escape_quotes(v)}" for v in value['value'])
+                    else:
+                        filter_value = f"{self._escape_quotes(value['value'])}"
+                    parsed_filters.append(f"{value['field']} {value['operator']} ({filter_value})")
+                else:
+                    raise ValueError(f"Unsupported operator: {value['operator']}")
+            elif key.startswith("operator_"):
+                parsed_filters.append(value)
+
+        return parsed_filters
+
+    def setup_selected_filters(self):
+        if self._selected_filters:
+            self.logger.info(f"Parsing '{self.name}' filters: {self._selected_filters}")
+            parsed_filters = self._parse_filters(self._selected_filters)
+            if parsed_filters and len(parsed_filters) > 0:
+                parsed_filters = self._join_filters(parsed_filters)
+                if self.custom_filter:
+                    self.custom_filter = f"{self.custom_filter} AND {parsed_filters}"
+                else:
+                    self.custom_filter = parsed_filters
+
 
 class NetsuiteDynamicSchema(NetSuiteStream):
     schema_response = None
